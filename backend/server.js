ba231ab825daab94821186b82239d05c1ea91ba9
Corina Conklin <corina0615@gmail.com>
2023-09-24 00:36:46 -0500

    made json local

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,8 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-let sampleData = {
-  temperature: '',
-  humidity: '',
-  weather: '',
-  groundPressure: '',
-  rain: '',
-  windspeed: ''
-};
+var sampleData = [];
 
-
-fs.readFile('data.json', 'utf8', (err, data) => {
-  if (!err) {
-    try {
-      sampleData = JSON.parse(data);
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
-    }
-  }
-});
 
 app.get('/', (req, res) => {
   res.send('Default Hello World');
@@ -35,28 +18,18 @@ app.get('/home', (req, res) => {
   res.send('Homepage send');
 });
 
-app.get('/data', (req, res) => {
-  res.json(sampleData);
-});
-
 app.get('/:name', (req, res) => {
   const name = req.params.name;
   res.send(name);
 });
 
+app.get('/data', (req, res) => {
+    res.json(sampleData);
+  });
+  
 app.post('/receive-json', (req, res) => {
   const receivedData = req.body;
-  sampleData = receivedData;
-  
-  // Save the received data to data.json
-  fs.writeFile('data.json', JSON.stringify(sampleData), 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing to data.json:', err);
-    } else {
-      console.log('JSON data received and saved successfully');
-    }
-  });
-
+  sampleData.push(receivedData);
   res.json({ message: 'JSON data received and saved successfully', data: receivedData });
 });
 
